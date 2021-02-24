cc9fdafdb6398bf63189ef09e7e1398768077269
Christopher Mitchell <76568996+CMitchell5619@users.noreply.github.com>
2021-02-24 13:32:18 -0700

    init

---
+++ app/AppState.js
@@ -0,0 +1,27 @@
+import Spell from "./Models/Spell.js"
+import Value from "./Models/Value.js"
+import { EventEmitter } from "./Utils/EventEmitter.js"
+import { isValidProp } from "./Utils/isValidProp.js"
+
+class AppState extends EventEmitter {
+  /** @type {Value[]} */
+  values = []
+  dndApiSpells = []
+  /**@type {Spell} */
+  activeSpell = null
+  /**@type {Spell[]} */
+  mySpells = []
+}
+
+export const ProxyState = new Proxy(new AppState(), {
+  get(target, prop) {
+    isValidProp(target, prop)
+    return target[prop]
+  },
+  set(target, prop, value) {
+    isValidProp(target, prop)
+    target[prop] = value
+    target.emit(prop, value)
+    return true
+  }
+})
---
+++ app/Services/AxiosService.js
@@ -0,0 +1,12 @@
+export const dndApi = axios.create({
+  baseURL: "https://www.dnd5eapi.co/api/spells",
+  timeout: 10000
+})
+
+
+
+export const sandboxApi = axios.create({
+  baseURL: 'https://bcw-sandbox.herokuapp.com/api/class/spells/',
+  timeout: 10000
+})
+
